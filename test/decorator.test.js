16928f42bcd7b8075f263b1834751313e6e16f2f
Nick Capps <nick.capps@viasat.com>
2020-01-06 18:07:48 -0800

    Add unit tests for decorator

--- test/decorator.test.js
+++ test/decorator.test.js
@@ -5,6 +5,38 @@ import Whip from '../src/decorator/whip';
 import HouseBlend from '../src/decorator/house-blend';
 import Soy from '../src/decorator/soy';
 
-test('test', () => {
-  expect('todo').toBe('todo');
+describe('beverage', () => {
+  const espresso = new Espresso();
+
+  it('getDescription', () => {
+    expect(espresso.getDescription()).toBe('Espresso');
+  });
+
+  it('getCost', () => {
+    expect(espresso.cost()).toBe(1.99);
+  });
+});
+
+describe('condiment decorator', () => {
+  let darkRoast = new DarkRoast();
+  darkRoast = new Mocha(darkRoast);
+  darkRoast = new Mocha(darkRoast);
+  darkRoast = new Whip(darkRoast);
+
+  it('getDescription', () => {
+    expect(darkRoast.getDescription()).toBe('Dark Roast Coffee, Mocha, Mocha, Whip');
+  });
+
+  it('getCost', () => {
+    expect(darkRoast.cost()).toBe(1.49);
+  });
+
+
+  let houseBlend = new HouseBlend();
+  houseBlend = new Soy(houseBlend);
+  houseBlend = new Mocha(houseBlend);
+  houseBlend = new Whip(houseBlend);
+  it('getSize', () => {
+    expect(houseBlend.cost()).toBe(1.29);
+  });
 });
